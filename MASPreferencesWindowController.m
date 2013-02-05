af2523997098e9a4e27d7e34ecd028023d379e24
Alan Rogers <alan@github.com>
2013-02-05 13:39:56 +1100

    MASPreferencesWindowController expects the view to be using resizing masks.
    (so we need to enable it on its view in case the VC is used elsewhere)

--- MASPreferencesWindowController.m
+++ MASPreferencesWindowController.m
@@ -271,6 +271,7 @@ static NSString *const PreferencesKeyForViewBounds (NSString *identifier)
     [[NSUserDefaults standardUserDefaults] setObject:controller.identifier forKey:kMASPreferencesSelectedViewKey];
     
     NSView *controllerView = controller.view;
+    controllerView.translatesAutoresizingMaskIntoConstraints = YES;
 
     // Retrieve current and minimum frame size for the view
     NSString *oldViewRectString = [[NSUserDefaults standardUserDefaults] stringForKey:PreferencesKeyForViewBounds(controller.identifier)];
